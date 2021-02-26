d54c3174319666692d583365efa912fa54b3a2b1
Matthias Knäpper <m.knaepper@it-knaepper.de>
2021-02-26 10:45:16 +0100

    Requesting headers and labels from json

--- X or M32 Current Firmware.js
+++ X or M32 Current Firmware.js
@@ -1,27 +1,30 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: light-gray; icon-glyph: mortar-pestle;
-// share-sheet-inputs: url, file-url;
 // Created by Matthias Knäpper, IT Knäpper, Dortmund;
 // 2021, Dortmund;
+// Some code comes from an Article of c't magazine. Thank you for the inspiration.
+// This widget requests the current firmware version of X/M32 mixing consoles
+// and the current version of the X/M32-Edit version (for macOS devices. Should be the same for
+// Windows and Linux devices as well)
 // Have Fun!;
 
 const timeFont = Font.boldSystemFont(12);
 const normalFont = Font.mediumSystemFont(12);
 const titleFont = Font.boldSystemFont(12);
 const smallFont = Font.mediumSystemFont(8);
-var url = "https://www.midasconsoles.com/.rest/musictribe/v1/downloadcenter/solr-dldatatable?brandName=midas&modelCode=P0B3I&type=Software&subtype=Firmware&catName=C-MIDAS-MIXINGCONSOLES-DIGITALMIXERS&sEcho=6&iColumns=6&sColumns=%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=15";
-var url2 = "https://www.midasconsoles.com/.rest/musictribe/v1/downloadcenter/solr-dldatatable?brandName=midas&modelCode=P0B3I&type=Software&subtype=Mac&catName=C-MIDAS-MIXINGCONSOLES-DIGITALMIXERS&sEcho=6&iColumns=6&sColumns=%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=15";
+var url = "https://www.midasconsoles.com/.rest/musictribe/v1/downloadcenter/solr-dldatatable?brandName=midas&cat-name=C-MIDAS-MIXINGCONSOLES-DIGITALMIXERS&modelCode=P0B3I&type=Software&subtype=Firmware&iDisplayStart=0&iDisplayLength=1000"
+var url2 = "https://www.midasconsoles.com/.rest/musictribe/v1/downloadcenter/solr-dldatatable?brandName=midas&cat-name=C-MIDAS-MIXINGCONSOLES-DIGITALMIXERS&modelCode=P0B3I&type=Software&subtype=Mac&iDisplayStart=0&iDisplayLength=1000";
+var ModelName;
+var SoftwareType1;
+var SoftwareTitle1;
+var SoftwareType2;
+var SoftwartTitle2;
+
 let data = await getData();
-//console.log(data);
 let widget = await createWidget(data);
 if (config.runsInWidget) {
   Script.setWidget(widget);
   Script.complete();
 } else {
-  console.log(printDate());
   await showTableHTML(data);
-  //Safari.open(`shortcuts://run-shortcut?name=SpringBoard`)
 }
 
 async function showTableHTML(midasversions) {
@@ -34,7 +37,7 @@ async function showTableHTML(midasversions) {
   span.start { text-decoration: underline;}
   td.stops { padding-bottom: 2em; }
 `;
-  let tr = taggify("tr",taggify("th", `Firmware`) + taggify("th", 'Software'));
+  let tr = taggify("tr",taggify("th", SoftwareTitle1) + taggify("th", SoftwartTitle2));
   tr += taggify("tr", taggify("td", `<a href=${mv.FirmwareDownloadUrl}>${mv.FirmwareVersion}</a>`, { class: "stops" }) 
      + taggify("td", `<a href=${mv.SoftwareDownloadUrl}>${mv.SoftwareVersion}</a>`, { class: "stops" }));
   tBody.push(tr);
@@ -46,7 +49,6 @@ async function showTableHTML(midasversions) {
   let view = new WebView();
   await view.loadHTML(html);
   let myview = await view.present();
-  //console.log(myview);
 }
 
 async function getData() {
@@ -54,6 +56,11 @@ async function getData() {
   let appReq = new Request(url2);
   let firmwaredata = await firmReq.loadJSON();
   let appdata = await appReq.loadJSON();
+  ModelName = `${ firmwaredata.aaData[0].modelName}`;
+  SoftwareType1 = `${firmwaredata.aaData[0].groupName}`;
+  SoftwareTitle1 = `${firmwaredata.aaData[0].title}`;
+  SoftwareType2 = `${appdata.aaData[0].groupName}`;
+  SoftwareTitle2 = `${appdata.aaData[0].title}`;
   let alldata = `{"FirmwareVersion":"${firmwaredata.aaData[0].releaseNotes}","FirmwareDownloadUrl":"${firmwaredata.aaData[0].url.url}","SoftwareVersion":"${appdata.aaData[0].releaseNotes}","SoftwareDownloadUrl":"${appdata.aaData[0].url.url}"}`;
   let version = new Array(firmwaredata.aaData[0].releaseNotes, appdata.aaData[0].releaseNotes);
   let midasobj = JSON.parse(alldata);
@@ -62,22 +69,21 @@ async function getData() {
 
 async function createWidget(midasversions) {
   let mv = JSON.parse(midasversions);
-  const backGradient = new LinearGradient();
+/*   const backGradient = new LinearGradient();
   backGradient.colors = [new Color("yellow"),new Color("white")];
   backGradient.locations = [0, 1];
   let files = FileManager.iCloud();
   const path = files.documentsDirectory() + "/header-logo-midas.svg";
-  console.log("Logopfad: " + path);
   const logoPNG = Image.fromFile(path);
-  
+ */  
   let widget = new ListWidget();
-//  widget.backgroundGradient = backGradient;
+  //widget.backgroundGradient = backGradient;
   //widget.backgroundImage = logoPNG;
   let stack = widget.addStack();
   stack.layoutVertically();
   
   let titleLine = stack.addStack();
-  let title = titleLine.addText("M32 Releases");
+  let title = titleLine.addText(ModelName + ": " + SoftwareType1);
   title.font = titleFont;
   titleLine.addSpacer();
  
@@ -85,9 +91,9 @@ async function createWidget(midasversions) {
   let firmware = stack.addStack();
   let softText = stack.addStack();
   let Software = stack.addStack();
-  let FirmText = firmText.addText('Firmware');
+  let FirmText = firmText.addText(SoftwareTitle1);
   let midfirmware = firmware.addText(`${mv.FirmwareVersion}`);
-  let SoftText = softText.addText('M32-Edit');
+  let SoftText = softText.addText(SoftwareTitle2);
   let midSoft = Software.addText(`${mv.SoftwareVersion}`);
   let spacer = stack.addStack();
   let dateTime = stack.addStack();
@@ -109,25 +115,17 @@ function taggify(el, str, atts) {
   }
   let res = `<${el}` + (atts ? " " + attributes.join(" ") : "")
     + `>${str}</${el}>`;
-  //console.log(res)
   return res
 }
 
 function printDate() {
   let datum = new Date();
-  console.log(datum);
   thisYear = datum.getFullYear();
-  console.log(thisYear);
   thisMonth = datum.getMonth()+1;
-  console.log(thisMonth);
   thisDay = datum.getDate();
-  console.log(thisDay);
   thisHour = datum.getHours();
-  console.log(thisHour);
   thisMinute = datum.getMinutes();
-  console.log(thisMinute);
   thisSecond = datum.getSeconds();
-  console.log(thisSecond);
   DatumString = `${thisYear}-${thisMonth}-${thisDay} ${thisHour}:${thisMinute}:${thisSecond}`;
   return DatumString
 }
